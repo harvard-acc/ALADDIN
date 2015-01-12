03e5341ad0b84caab45acfe9574868ad36f8c5ed
Sam Xi <samxi@seas.harvard.edu>
2015-01-12 17:01:58 -0500

    Put all unit test outputs under an "outputs" directory.
    Fix an error with building the Aladdin static library.
    
    Change-Id: If64516744c6160f98ec696b45e48dea269df0036

--- unit-test/test_spm_part.cpp
+++ unit-test/test_spm_part.cpp
@@ -10,7 +10,7 @@ SCENARIO("Test scratchpadPartition w/ Triad", "[triad]")
   GIVEN("Test Triad w/ Input Size 128, cyclic partition with a factor of 2, "
         "loop unrolling with a factor of 2, enable loop pipelining")
   {
-    std::string bench("triad-128");
+    std::string bench("outputs/triad-128");
     std::string trace_file("inputs/triad-128-trace");
     std::string config_file("inputs/config-triad-p2-u2-P1");
 
@@ -48,7 +48,7 @@ SCENARIO("Test scratchpadPartition w/ Reduction", "[reduction]")
 {
   GIVEN("Test Reduction w/ Input Size 128")
   {
-    std::string bench("reduction-128");
+    std::string bench("outputs/reduction-128");
     std::string trace_file("inputs/reduction-128-trace");
     std::string config_file("inputs/config-reduction-p4-u4-P1");
 
@@ -79,7 +79,7 @@ SCENARIO("Test scratchpadPartition w/ pp_scan", "[pp_scan]")
 {
   GIVEN("Test pp_scan w/ Input Size 128")
   {
-    std::string bench("pp_scan-128");
+    std::string bench("outputs/pp_scan-128");
     std::string trace_file("inputs/pp_scan-128-trace");
     std::string config_file("inputs/config-pp_scan-p4-u4-P1");
 
