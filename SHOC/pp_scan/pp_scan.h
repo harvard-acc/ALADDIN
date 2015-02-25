3c1423d9fb468f00496d3e894f47f7f23193854b
Rafael Garibotti <rgaribotti@seas.harvard.edu>
2015-02-25 18:21:45 -0500

    Update config.py script. Variable sum (pp_scan) is a vector of 128-size, instead of 16-size. Moreover, it was removed some commented lines in the same benchmark.
    Change-Id: Ib4d6a24f6517f2043502140fc2494fa866c604d7

--- SHOC/pp_scan/pp_scan.h
+++ SHOC/pp_scan/pp_scan.h
@@ -4,11 +4,7 @@
 #define TYPE int
 
 #define N 2048
-#define NUMOFBLOCKS 512
-//#define N 32
-//#define NUMOFBLOCKS 8
-#define RADIXSIZE 16
-
 #define SCAN_BLOCK 16
+
 #define SCAN_RADIX N/SCAN_BLOCK
 #define BUCKETSIZE SCAN_BLOCK*SCAN_RADIX
