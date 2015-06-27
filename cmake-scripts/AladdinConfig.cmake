6c468d5681406d71c40571fcddfb96129d2a66e0
Chia-Wei Chang <rglyab@gmail.com>
2015-06-27 21:12:38 +0800

    Support LLVM-Tracer at outside of Aladdin dir
    The original design requires LLVM-Tracer under Aladdin's root path.
    The CMake sciprt can search the pathc of LLVM-Tracer according to
    given CMake variable TRACER_DIR or environment variable
    TRACER_HOME. The Default Tracer path is /where/Aladdin/LLVM-Tracer.

---
+++ cmake-scripts/AladdinConfig.cmake
@@ -0,0 +1,20 @@
+set(HAS_ALADDIN TRUE)
+
+# settings for LLVM-Tracer
+if(NOT DEFINED BUILD_ON_SOURCE)
+  set(BUILD_ON_SOURCE FALSE)
+endif()
+
+# Check whether LLVM-Tracer exists
+if(DEFINED TRACER_DIR)
+  get_filename_component(TRACER_DIR ${TRACER_DIR} ABSOLUTE)
+elseif("$ENV{TRACER_HOME}" STREQUAL "")
+  set(TRACER_DIR "${CMAKE_SOURCE_DIR}/LLVM-Tracer")
+else()
+  set(TRACER_DIR "$ENV{TRACER_HOME}")
+endif()
+
+
+if(NOT EXISTS ${TRACER_DIR})
+  message(FATAL_ERROR "finds no ${TRACER_DIR}")
+endif()
