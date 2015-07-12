66d3ba07505987cade764f942b20ffe0dbfb2774
Chia-Wei Chang <rglyab@gmail.com>
2015-07-12 13:07:19 +0800

    insert root_dir to input files of unit-tests (2)
    Pass INPUT_ROOT_DIR=${CMAKE_CURRENT_SOURCE_DIR} to unit-tests, and
    the default value is "." to make the legacy makefile works.

--- unit-test/unit-test-config.h
+++ unit-test/unit-test-config.h
@@ -4,3 +4,14 @@
 #include "file_func.h"
 #include "Scratchpad.h"
 #include "ScratchpadDatapath.h"
+
+#ifndef INPUT_ROOT_DIR
+  #define INPUT_ROOT_DIR .
+#endif
+
+// turn foo into "foo"
+// reference: http://stackoverflow.com/questions/240353/convert-a-preprocessor-token-to-a-string
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+const std::string root_dir = std::string(TOSTRING(INPUT_ROOT_DIR)) + "/";
