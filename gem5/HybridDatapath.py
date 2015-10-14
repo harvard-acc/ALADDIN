1a53a238d3c2e3d58cd23857e2f42f60538ec7e7
Sophia Shao <shao@eecs.harvard.edu>
2015-10-14 15:10:29 -0400

    Connect cache_port and bus directly when we call addPrivateL1DCache().
    Instead of having a shared list across all the instances.
    
    Change-Id: I0146896ec7ed3f4a7d7fb9fea2536f2e8b5385fe

--- gem5/HybridDatapath.py
+++ gem5/HybridDatapath.py
@@ -54,37 +54,12 @@ class HybridDatapath(MemObject):
 
   spad_port = MasterPort("HybridDatapath DMA port")
   cache_port = MasterPort("HybridDatapath cache coherent port")
-  # This is the set of ports that should be connected to the memory bus.
-  _cached_ports = ['spad_port', "cache_port"]
-  _uncached_slave_ports = []
-  _uncached_master_ports = []
+  # HACK: We don't have a scratchpad object. Currently we just connect the
+  # scratchpad port inside datapath directly to the memory bus.
+  def connectPrivateScratchpad(self, bus):
+    self.spad_port = bus.slave
 
-  def connectCachedPorts(self, bus):
-      for p in self._cached_ports:
-          exec('self.%s = bus.slave' % p)
-
-  def connectUncachedPorts(self, bus):
-      for p in self._uncached_slave_ports:
-          exec('self.%s = bus.master' % p)
-      for p in self._uncached_master_ports:
-          exec('self.%s = bus.slave' % p)
-
-  def connectAllPorts(self, cached_bus, uncached_bus = None) :
-    self.connectCachedPorts(cached_bus)
-    if not uncached_bus:
-      uncached_bus = cached_bus
-    self.connectUncachedPorts(uncached_bus)
-
-  def addPrivateScratchpad(self, spad, dwc = None) :
-    self.spad = spad
-    self.spad_port = spad.cpu_side
-    # The spad port is now connected to the scratchpad itself, so it should not
-    # be reconnected to the memory bus later.
-    self._cached_ports.remove("spad_port")
-    self._cached_ports.append('spad.mem_side')
-
-  def addPrivateL1Dcache(self, cache, dwc = None) :
+  def addPrivateL1Dcache(self, cache, bus, dwc = None) :
     self.cache = cache
     self.cache_port = cache.cpu_side
-    self._cached_ports.remove("cache_port")
-    self._cached_ports.append('cache.mem_side')
+    self.cache.mem_side  = bus.slave
