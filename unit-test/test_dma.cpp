c0150da40d36d3875358727e26f7d4c421a73a6f
Sam Xi <samxi@seas.harvard.edu>
2017-01-04 17:05:11 -0500

    dma: Unit test for DMA interface v2 and double buffering.
    This tests dependency management and optimization with the new separated
    source and destination offsets and DMA fences.
    
    Change-Id: I56390b4ac9f93ff686e73d49e316b55eb9e03865

--- unit-test/test_dma.cpp
+++ unit-test/test_dma.cpp
@@ -59,3 +59,131 @@ SCENARIO("Test DMA Dependence w/ Triad", "[triad]") {
     }
   }
 }
+
+SCENARIO("Test double buffering memcpy", "[double-buffering]") {
+  GIVEN("A simple, single-iteration of a double buffered memcpy, "
+        "input size 128 integers, with unrolling factor 2 and "
+        "partitioning factor 1") {
+    std::string bench("outputs/double-buffering");
+    std::string trace_file("inputs/double_buffering_trace.gz");
+    std::string config_file("inputs/double_buffering.cfg");
+
+    ScratchpadDatapath* acc;
+    Scratchpad* spad;
+    acc = new ScratchpadDatapath(bench, trace_file, config_file);
+    acc->buildDddg();
+    acc->removeInductionDependence();
+    acc->removePhiNodes();
+    acc->initBaseAddress();
+    acc->scratchpadPartition();
+
+    // Location of important nodes.
+    unsigned dmaLoad1 = 1;
+    unsigned dmaLoad2 = 2;
+    unsigned dmaLoad3 = 527;
+    unsigned dmaLoad4 = 1048;
+    unsigned dmaStore1 = 520;
+    unsigned dmaStore2 = 1043;
+    unsigned dmaStore3 = 1569;
+    unsigned dmaStore4 = 2087;
+    unsigned dmaFence1 = 524;
+    unsigned dmaFence2 = 1045;
+    unsigned firstLoopLoad = 9;
+    unsigned secondLoopLoad = 531;
+    unsigned firstLoopStore = 11;
+    unsigned secondLoopStore = 533;
+
+    WHEN("After building the initial DDDG") {
+      THEN("The first two DMA ops are only dependent on memory operations and "
+           "fences.") {
+        REQUIRE(acc->getNumOfConnectedNodes(dmaLoad1) == 65);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaLoad2) == 65);
+
+        THEN("The following DMA loads and stores are also dependent on index "
+             "computations.") {
+          acc->dumpGraph("double_buffering");
+          // First dmaStore is connected to ONE fence (which immediately
+          // precedes it) and ONE index computation.
+          REQUIRE(acc->getNumOfConnectedNodes(dmaStore1) == 66);
+          REQUIRE(acc->doesEdgeExist(dmaStore1 - 1, dmaStore1));
+          REQUIRE(acc->doesEdgeExist(dmaStore1, dmaFence1));
+          // Second dmaStore is connected to TWO fences and ONE index
+          // computation.
+          REQUIRE(acc->getNumOfConnectedNodes(dmaStore2) == 67);
+          REQUIRE(acc->doesEdgeExist(dmaStore2 - 1, dmaStore2));
+          REQUIRE(acc->doesEdgeExist(dmaFence1, dmaStore2));
+          REQUIRE(acc->doesEdgeExist(dmaStore2, dmaFence2));
+        }
+      }
+      THEN("None of the DMA ops are isolated.") {
+        REQUIRE(acc->getNumOfConnectedNodes(dmaLoad4) > 0);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaStore2) > 0);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaStore3) > 0);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaStore4) > 0);
+      }
+      THEN("The loads in the memcpy are only connected to dmaLoads, GEPs, "
+           "and stores.") {
+        for (unsigned node_id = firstLoopLoad; node_id < dmaStore1;
+             node_id += 8) {
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 3);
+          REQUIRE(acc->doesEdgeExist(dmaLoad1, node_id) == true);
+          REQUIRE(acc->doesEdgeExist(dmaLoad2, node_id) == false);
+        }
+        for (unsigned node_id = secondLoopLoad; node_id < dmaStore2;
+             node_id += 8) {
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 3);
+          REQUIRE(acc->doesEdgeExist(dmaLoad2, node_id) == true);
+          REQUIRE(acc->doesEdgeExist(dmaLoad1, node_id) == false);
+          REQUIRE(acc->doesEdgeExist(node_id, dmaStore1) == false);
+        }
+      }
+      THEN("The stores in the memcpy are only connected to dmaStores, GEPs, "
+           "and loads.") {
+        for (unsigned node_id = firstLoopStore; node_id < dmaStore1;
+             node_id += 8) {
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 3);
+          REQUIRE(acc->doesEdgeExist(node_id, dmaStore1) == true);
+          REQUIRE(acc->doesEdgeExist(node_id, dmaStore2) == false);
+          REQUIRE(acc->doesEdgeExist(dmaLoad1, node_id) == false);
+          REQUIRE(acc->doesEdgeExist(dmaLoad2, node_id) == false);
+        }
+        for (unsigned node_id = secondLoopStore; node_id < dmaStore2;
+             node_id += 8) {
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 3);
+          REQUIRE(acc->doesEdgeExist(node_id, dmaStore2) == true);
+          REQUIRE(acc->doesEdgeExist(dmaLoad1, node_id) == false);
+          REQUIRE(acc->doesEdgeExist(dmaLoad2, node_id) == false);
+          REQUIRE(acc->doesEdgeExist(node_id, 517) == false);
+        }
+      }
+    }
+    WHEN("After applying loop unrolling") {
+      acc->loopUnrolling();
+      THEN("The number of connected nodes to DMA loads/stores should not "
+           "change.") {
+        REQUIRE(acc->getNumOfConnectedNodes(dmaLoad1) == 65);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaLoad2) == 65);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaStore1) == 66);
+        REQUIRE(acc->getNumOfConnectedNodes(dmaStore2) == 67);
+      }
+      THEN(
+          "The loads in the memcpy are connected to two more branch nodes.") {
+        for (unsigned node_id = firstLoopLoad; node_id < dmaStore1;
+             node_id += 8)
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 5);
+        for (unsigned node_id = secondLoopLoad; node_id < dmaStore2;
+             node_id += 8)
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 5);
+      }
+      THEN("The stores in the memcpy are also connected to two more "
+           "branches.") {
+        for (unsigned node_id = firstLoopStore; node_id < dmaStore1;
+             node_id += 8)
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 5);
+        for (unsigned node_id = secondLoopStore; node_id < dmaStore2;
+             node_id += 8)
+          REQUIRE(acc->getNumOfConnectedNodes(node_id) == 5);
+      }
+    }
+  }
+}
