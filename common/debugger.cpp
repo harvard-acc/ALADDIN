ed300c8b473f0ebcec1ee080dfcfde75d1f7b22e
Sam Xi <samxi@seas.harvard.edu>
2017-06-24 17:27:04 -0400

    debugger: Add a debugger for Aladdin.
    Using gdb to debug issues with the DDDG is incredibly difficult and
    tedious (not all functions are visible to or callable from gdb), and
    rendering the entire DDDG is basically impossible for any graph
    visualization software I know of. So instead, I wrote an interactive
    debugger that will print information about nodes and the graph. It works
    as a drop-in replacement for Aladdin, but it will interrupt the flow of
    Aladdin at specific places to allow the user to query for information.
    
    Change-Id: I2c5cb44d4098ffad6e35395277d6d85fe8e70d73

---
+++ common/debugger.cpp
@@ -0,0 +1,468 @@
+// A debugging interface for Aladdin.
+//
+// The debugger makes it much simpler to inspect the DDDG and find out why
+// Aladdin is not providing expected performance/power/area estimates. It can
+// print information about individual nodes, including all of its parents and
+// children (which cannot be easily deduced by looking at the trace alone). It
+// can also dump a subgraph of the DDDG in Graphviz format, making visual
+// inspection possible (as the entire DDDG is too large to visualize).
+//
+// This is a drop in replacement for Aladdin, so run it just like standalone
+// Aladdin but with the different executable name.
+//
+// For more information, see the help command.
+
+#include "file_func.h"
+#include "ScratchpadDatapath.h"
+#include "Scratchpad.h"
+#include "DDDG.h"
+#include <stdio.h>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/tokenizer.hpp>
+#include <exception>
+
+enum DebugReturnCode {
+  CONTINUE,
+  QUIT,
+  ERR
+};
+
+class bfs_finished : public std::exception {
+  virtual const char* what() const throw() {
+    return "BFS finished";
+  }
+};
+
+class NodeVisitor : public boost::default_bfs_visitor {
+ public:
+  NodeVisitor(Graph* _subgraph,
+              std::map<unsigned, Vertex>* _existing_nodes,
+              ScratchpadDatapath* _acc,
+              Vertex _root_vertex,
+              unsigned maxnodes,
+              unsigned* _num_nodes_visited)
+      : new_graph(_subgraph), existing_nodes(_existing_nodes), acc(_acc),
+        root_vertex(_root_vertex), max_nodes(maxnodes),
+        num_nodes_visited(_num_nodes_visited) {}
+
+  Vertex insert_vertex(unsigned node_id) const {
+    if (*num_nodes_visited >= max_nodes)
+      throw bfs_finished();
+
+    return add_vertex(VertexProperty(node_id), *new_graph);
+  }
+
+  void discover_vertex(const Vertex& v, const Graph& g) const {
+    unsigned node_id = get(boost::vertex_node_id, g, v);
+    auto node_it = existing_nodes->find(node_id);
+    Vertex new_vertex;
+    if (node_it == existing_nodes->end()) {
+      new_vertex = insert_vertex(node_id);
+      existing_nodes->operator[](node_id) = new_vertex;
+      (*num_nodes_visited)++;
+    } else {
+      new_vertex = existing_nodes->at(node_id);
+    }
+
+    // If this node is a branch/call, don't print its children (unless this
+    // node is the root of the BFS). These nodes tend to have a lot of children.
+    if (new_vertex != root_vertex) {
+      ExecNode* node = acc->getNodeFromNodeId(node_id);
+      if (node->is_branch_op() || node->is_call_op())
+        return;
+    }
+
+    // Immediately add all of its outgoing edges to the graph.
+    out_edge_iter edge_it, end;
+    for (boost::tie(edge_it, end) = out_edges(v, g); edge_it != end; ++edge_it) {
+      Vertex target_vertex_orig = target(*edge_it, g);
+      unsigned target_id = get(boost::vertex_node_id, g, target_vertex_orig);
+      if (existing_nodes->find(target_id) == existing_nodes->end()) {
+        Vertex target_vertex = insert_vertex(target_id);
+        unsigned edge_weight = get(boost::edge_name, g, *edge_it);
+        add_edge(
+            new_vertex, target_vertex, EdgeProperty(1), *new_graph);
+        existing_nodes->operator[](target_id) = target_vertex;
+        (*num_nodes_visited)++;
+      }
+    }
+  }
+  void initialize_vertex(const Vertex& v, const Graph& g) const {}
+  void examine_vertex(const Vertex& v, const Graph& g) const {}
+  void examine_edge(const Edge& e, const Graph& g) const {}
+  void finish_vertex(const Vertex& v, const Graph& g) const {}
+
+ private:
+  std::map<unsigned, Vertex> *existing_nodes;
+  Graph* new_graph;
+  ScratchpadDatapath* acc;
+  Vertex root_vertex;
+  unsigned max_nodes;
+  unsigned* num_nodes_visited;
+};
+
+DebugReturnCode cmd_print(std::vector<std::string>& command_tokens,
+                         ScratchpadDatapath* acc) {
+
+  unsigned num_tokens = command_tokens.size();
+  if (num_tokens < 2) {
+    std::cerr << "ERROR: Invalid arguments to 'print'.\n";
+    return ERR;
+  }
+  std::string print_type = command_tokens[1];
+  if (print_type == "node") {
+    if (num_tokens == 2) {
+      std::cerr << "ERROR: Need to specify a node id!\n";
+      return ERR;
+    }
+    unsigned node_id = 0xdeadbeef;
+    try {
+      node_id = std::stoi(command_tokens[2], NULL, 10);
+    } catch (const std::invalid_argument &e) {
+      std::cerr << "ERROR: Invalid node id! Must be a nonnegative integer.\n";
+      return ERR;
+    }
+
+    if (!acc->doesNodeExist(node_id)) {
+      std::cerr << "ERROR: Node " << node_id << " does not exist!\n";
+      return ERR;
+    }
+    ExecNode* node = acc->getNodeFromNodeId(node_id);
+
+    std::cout << "Node " << node_id << ":\n"
+              << "  Opcode: " << node->get_microop_name() << "\n";
+
+    SourceManager& srcManager = acc->get_source_manager();
+    int line_num = node->get_line_num();
+    if (line_num != -1)
+      std::cout << "  Line number: " << line_num << "\n";
+
+    // What function does this node belong to?
+    Function func = srcManager.get<Function>(node->get_static_function_id());
+    if (func != InvalidFunction) {
+      std::cout << "  Parent function: " << func.get_name() << "\n";
+      std::cout << "  Dynamic invocation count: " << node->get_dynamic_invocation() << "\n";
+    }
+    if (line_num != -1 && func != InvalidFunction) {
+      const std::multimap<unsigned, UniqueLabel>& labelmap = acc->getLabelMap();
+      auto label_range = labelmap.equal_range(line_num);
+      for (auto it = label_range.first; it != label_range.second; ++it) {
+        if (it->second.get_function_id() == func.get_id()) {
+          src_id_t label_id = it->second.get_label_id();
+          Label label = srcManager.get<Label>(label_id);
+          std::cout << "  Code label: " << label.get_name() << "\n";
+          break;
+        }
+      }
+    }
+
+    if (node->is_memory_op()) {
+      MemAccess *mem_access = node->get_mem_access();
+      std::cout << "  Memory access to array " << node->get_array_label() << "\n"
+                << "    Addr:  0x" << std::hex << mem_access->vaddr << "\n"
+                << "    Size:  " << std::dec << mem_access->size << "\n";
+      std::cout << "    Value: ";
+      if (mem_access->is_float && mem_access->size == 4)
+        std::cout << FP2BitsConverter::ConvertBitsToFloat(mem_access->value);
+      else if (mem_access->is_float && mem_access->size == 8)
+        std::cout << FP2BitsConverter::ConvertBitsToDouble(mem_access->value);
+      else
+        std::cout << mem_access->value;
+      std::cout << "\n";
+    }
+    if (node->get_microop() == LLVM_IR_GetElementPtr) {
+      std::cout << "  GEP of array " << node->get_array_label() << "\n";
+    }
+    std::cout << "  Inductive: ";
+    if (node->is_inductive())
+      std::cout << "Yes\n";
+    else
+      std::cout << "No\n";
+
+    // Print information about the variables involved, if info exists.
+    Variable var = srcManager.get<Variable>(node->get_variable_id());
+    if (var != InvalidVariable)
+      std::cout << "  Variable: " << var.str() << "\n";
+
+    // For Call nodes, the next node will indicate what the called function is
+    // (unless the called function is empty).
+    if (node->is_call_op()) {
+      ExecNode* called_node = NULL;
+      try {
+        called_node = acc->getNodeFromNodeId(node_id + 1);
+      } catch (const std::out_of_range &e) {}
+
+      std::cout << "  Called function: ";
+      if (called_node) {
+        Function called_func =
+            srcManager.get<Function>(called_node->get_static_function_id());
+        if (called_func != func && called_func != InvalidFunction)
+          std::cout << called_func.get_name() << "\n";
+        else
+          std::cout << "Unable to determine.\n";
+      } else {
+        std::cout << "Unable to determine.\n";
+      }
+    }
+
+    // Print the list of all parent nodes and child nodes.
+    std::vector<unsigned> parentNodes = acc->getParentNodes(node_id);
+    std::cout << "  Parents: " << parentNodes.size() << " [ ";
+    for (auto parent_node : parentNodes) {
+      std::cout << parent_node << " ";
+    }
+    std::cout << "]\n";
+    std::vector<unsigned> childNodes = acc->getChildNodes(node_id);
+    std::cout << "  Children: " << childNodes.size() << " [ ";
+    for (auto child_node : childNodes) {
+      std::cout << child_node << " ";
+    }
+    std::cout << "]\n";
+  } else {
+    std::cerr << "ERROR: Unsupported object to print!\n";
+    return ERR;
+  }
+  return CONTINUE;
+}
+
+void dump_graph(Graph& graph, ScratchpadDatapath* acc, std::string graph_name) {
+  std::unordered_map<Vertex, unsigned> vertexToMicroop;
+  BGL_FORALL_VERTICES(v, graph, Graph) {
+    ExecNode* node = acc->getNodeFromNodeId(get(boost::vertex_node_id, graph, v));
+    vertexToMicroop[v] = node->get_microop();
+  }
+  std::ofstream out(graph_name + "_graph.dot", std::ofstream::out);
+  write_graphviz(out, graph, make_microop_label_writer(vertexToMicroop, graph));
+}
+
+void reconstruct_graph(Graph* new_graph,
+                       ScratchpadDatapath* acc,
+                       unsigned root_node_id,
+                       unsigned maxnodes) {
+  const Graph &g = acc->getGraph();
+  ExecNode* root_node = acc->getNodeFromNodeId(root_node_id);
+  Vertex root_vertex = root_node->get_vertex();
+  unsigned num_nodes_visited = 0;
+  std::map<unsigned, Vertex> existing_nodes;
+  NodeVisitor visitor(new_graph,
+                      &existing_nodes,
+                      acc,
+                      root_vertex,
+                      maxnodes,
+                      &num_nodes_visited);
+  boost::breadth_first_search(g, root_vertex, boost::visitor(visitor));
+}
+
+// graph root=N [maxnodes=K]
+DebugReturnCode cmd_graph(std::vector<std::string>& command_tokens,
+                          ScratchpadDatapath* acc) {
+  if (command_tokens.size() < 2) {
+    std::cout << "Invalid arguments to command 'graph'!\n";
+    return ERR;
+  }
+
+  int root_node = -1;
+  int maxnodes = -1;
+
+  boost::char_separator<char> sep("=");
+  for (unsigned i = 1; i < command_tokens.size(); i++) {
+    boost::tokenizer<boost::char_separator<char>> tok(command_tokens[i], sep);
+    std::string arg;
+    int value = -1;
+    int argnum = 0;
+    for (auto it = tok.begin(); it != tok.end(); ++it, ++argnum) {
+      if (argnum == 0) {
+        arg = *it;
+        continue;
+      } else if (argnum == 1) {
+        try {
+          value = stoi(*it);
+        } catch (const std::invalid_argument &e) {
+          std::cerr << "ERROR: Invalid argument " << *it << " to parameter " << arg << ".\n";
+          return ERR;
+        }
+      }
+      if (value == -1) {
+        std::cerr << "ERROR: Missing value to parameter " << arg << ".\n";
+        return ERR;
+      }
+      if (arg == "root") {
+        root_node = value;
+      } else if (arg == "maxnodes") {
+        maxnodes = value;
+      } else {
+        std::cerr << "ERROR: Unknown parameter " << arg << " to command 'graph'.\n";
+        return ERR;
+      }
+    }
+  }
+
+  if (root_node == -1) {
+    std::cerr << "ERROR: Must specify the root node!\n";
+    return ERR;
+  }
+
+  // Set default values for missing parameters.
+  if (maxnodes == -1)
+    maxnodes = 100;
+
+  if (!acc->doesNodeExist(root_node)) {
+    std::cerr << "ERROR: Node " << root_node << " does not exist!\n";
+    return ERR;
+  }
+  Graph subgraph;
+  try {
+    reconstruct_graph(&subgraph, acc, root_node, maxnodes);
+  } catch (const bfs_finished &e) {
+  }
+  dump_graph(subgraph, acc, "debug");
+  std::cout << "Graph has been written to debug_graph.dot.\n";
+  return CONTINUE;
+}
+
+void cmd_help() {
+  std::cout << "\nAladdin debugger help\n"
+            << "========================\n\n"
+            << "The Aladdin debugger works just like Aladdin itself, except after Aladdin runs the\n"
+            << "global optimization pass, it will pause and allow the user to query information\n"
+            << "about the DDDG. The user can print details about individual nodes and see them in\n"
+            << "a format that is more readable than the raw trace. Additionally, the user can\n"
+            << "request a dump of a portion of the DDDG to visually inspect the dependencies\n"
+            << "between nodes, as the entire DDDG is generally too large for any graph drawing\n"
+            << "program to render.\n\n"
+            << "The debugger will interrupt Aladdin's execution at two places: after the global\n"
+            << "optimization pass, and after all the scheduling has taken place.\n\n"
+            << "Note: the debugger cannot alter any Aladdin state. It can only report the state\n"
+            << "in its current form.\n\n"
+            << "Supported commands:\n"
+            << "  help                               : Print this message\n"
+            << "  print node [id]                    : Print details about this node\n"
+            << "  graph root=node-id <maxnodes=N>    : Dump the DDDG in BFS fashion, starting from\n"
+            << "         the specified root node. maxnodes indicates the maximum number of nodes to\n"
+            << "         dump (since large graphs can cause rendering programs to choke. If maxnodes\n"
+            << "         is not specified, it defaults to 100.\n\n"
+            << "         NOTE: Branch and call nodes tend to have a lot of child dependent nodes\n"
+            << "         that are usually not dependent on each other (e.g. different iterations of the\n"
+            << "         same or different loop). So, unless it is the specified root node, the children\n"
+            << "         of branch and call nodes will not get printed.\n"
+            << "  continue                           : Continue executing Aladdin\n"
+            << "  quit                               : Quit the debugger.\n";
+}
+
+void cmd_unknown(std::string& command) {
+  std::cout << "\nUnknown command " << command << std::endl;
+}
+
+// Supported commands.
+//
+// help
+// print node <id>
+// print array <array name>  [ PLANNED ]
+// graph <root node> <maxnodes>
+// continue
+// quit
+DebugReturnCode interactive_mode(ScratchpadDatapath* acc) {
+  std::cout << "Entering Aladdin Debugger...\n";
+  while (true) {
+    std::cout << "aladdin >> ";
+    std::string command;
+    std::getline(std::cin, command);
+
+    if (command.empty()) {
+      continue;
+    }
+
+    boost::char_separator<char> sep(" ");
+    boost::tokenizer<boost::char_separator<char>> tok(command, sep);
+    std::vector<std::string> command_split;
+    unsigned arg_num = 0;
+    for (auto it = tok.begin(); it != tok.end(); ++it) {
+      command_split.push_back(*it);
+    }
+
+    if (command_split.size() == 0) {
+      continue;
+    }
+
+    // Execute the command.
+    std::string root_command = command_split[0];
+    if (root_command == "quit") {
+      return QUIT;
+    } else if (root_command == "continue") {
+      return CONTINUE;
+    } else if (root_command == "print") {
+      cmd_print(command_split, acc);
+    } else if (root_command == "graph") {
+      cmd_graph(command_split, acc);
+    } else if (root_command == "help") {
+      cmd_help();
+    } else {
+      cmd_unknown(command);
+    }
+  }
+}
+
+int main(int argc, const char* argv[]) {
+  const char* logo =
+      "     ________                                                    \n"
+      "    /\\ ____  \\    ___   _       ___  ______ ______  _____  _   _ \n"
+      "   /  \\    \\  |  / _ \\ | |     / _ \\ |  _  \\|  _  \\|_   _|| \\ | "
+      "|\n"
+      "  / /\\ \\    | | / /_\\ \\| |    / /_\\ \\| | | || | | |  | |  |  \\| "
+      "|\n"
+      " | |  | |   | | |  _  || |    |  _  || | | || | | |  | |  | . ` |\n"
+      " \\ \\  / /__/  | | | | || |____| | | || |/ / | |/ /  _| |_ | |\\  |\n"
+      "  \\_\\/_/ ____/  \\_| |_/\\_____/\\_| |_/|___/  |___/  |_____|\\_| "
+      "\\_/\n"
+      "                                                                 \n";
+
+  std::cout << logo << std::endl;
+
+  if (argc < 4) {
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "Aladdin Debugger Usage:    " << std::endl;
+    std::cout
+        << "./debugger <bench> <dynamic trace> <config file>"
+        << std::endl;
+    std::cout << "   Aladdin supports gzipped dynamic trace files - append \n"
+              << "   the \".gz\" extension to the end of the trace file."
+              << std::endl;
+    std::cout << "-------------------------------" << std::endl;
+    exit(0);
+  }
+
+  std::string bench(argv[1]);
+  std::string trace_file(argv[2]);
+  std::string config_file(argv[3]);
+
+  std::cout << bench << "," << trace_file << "," << config_file << ","
+            << std::endl;
+
+  ScratchpadDatapath* acc;
+
+  acc = new ScratchpadDatapath(bench, trace_file, config_file);
+
+  // Build the graph.
+  acc->buildDddg();
+  acc->globalOptimizationPass();
+  acc->prepareForScheduling();
+
+  // Begin interactive mode.
+  DebugReturnCode ret = interactive_mode(acc);
+  if (ret == QUIT) {
+    goto exit;
+  }
+
+  // Scheduling
+  while (!acc->step()) {}
+  acc->dumpStats();
+
+  // Begin interactive mode again.
+  ret = interactive_mode(acc);
+
+  acc->clearDatapath();
+
+exit:
+  delete acc;
+  return 0;
+}
