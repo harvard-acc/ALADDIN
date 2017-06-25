645d204f226690f51fdb806164e2782e835ca53c
Sam Xi <samxi@seas.harvard.edu>
2017-06-25 15:28:46 -0400

    debugger: Add parent edges to nodes if the parent is a branch/call.
    This eliminates most of the isolated subgraphs in the resulting graph
    dump. These exist because we don't normally add the children of
    branches, but those children may get visited by the BFS algorithm later
    anyways, and therefore an edge to the parent branch/call would be
    missing.
    
    Change-Id: I85d7ef4c86496074c52e7e909f02e3cdc60293bd

--- common/debugger.cpp
+++ common/debugger.cpp
@@ -45,6 +45,7 @@ class NodeVisitor : public boost::default_bfs_visitor {
         root_vertex(_root_vertex), max_nodes(maxnodes),
         num_nodes_visited(_num_nodes_visited) {}
 
+  // Insert a vertex with node_id if we aren't above the max_nodes limit.
   Vertex insert_vertex(unsigned node_id) const {
     if (*num_nodes_visited >= max_nodes)
       throw bfs_finished();
@@ -52,6 +53,7 @@ class NodeVisitor : public boost::default_bfs_visitor {
     return add_vertex(VertexProperty(node_id), *new_graph);
   }
 
+  // Add a vertex and all of its children to the graph.
   void discover_vertex(const Vertex& v, const Graph& g) const {
     unsigned node_id = get(boost::vertex_node_id, g, v);
     auto node_it = existing_nodes->find(node_id);
@@ -72,25 +74,42 @@ class NodeVisitor : public boost::default_bfs_visitor {
         return;
     }
 
+    // Check if any of its parents is a branch/call whose children we ignored.
+    // If so, add an edge for each parent we already saw.
+    in_edge_iter in_edge_it, in_edge_end;
+    for (boost::tie(in_edge_it, in_edge_end) = in_edges(v, g);
+         in_edge_it != in_edge_end;
+         ++in_edge_it) {
+      Vertex source_vertex_orig = source(*in_edge_it, g);
+      unsigned source_id = get(boost::vertex_node_id, g, source_vertex_orig);
+      auto source_it = existing_nodes->find(source_id);
+      if (source_it != existing_nodes->end()) {
+        Vertex source_vertex_new = source_it->second;
+        unsigned edge_weight = get(boost::edge_name, g, *in_edge_it);
+        add_edge(source_vertex_new,
+                 new_vertex,
+                 EdgeProperty(edge_weight),
+                 *new_graph);
+      }
+    }
+
     // Immediately add all of its outgoing edges to the graph.
-    out_edge_iter edge_it, end;
-    for (boost::tie(edge_it, end) = out_edges(v, g); edge_it != end; ++edge_it) {
-      Vertex target_vertex_orig = target(*edge_it, g);
+    out_edge_iter out_edge_it, out_edge_end;
+    for (boost::tie(out_edge_it, out_edge_end) = out_edges(v, g);
+         out_edge_it != out_edge_end;
+         ++out_edge_it) {
+      Vertex target_vertex_orig = target(*out_edge_it, g);
       unsigned target_id = get(boost::vertex_node_id, g, target_vertex_orig);
       if (existing_nodes->find(target_id) == existing_nodes->end()) {
         Vertex target_vertex = insert_vertex(target_id);
-        unsigned edge_weight = get(boost::edge_name, g, *edge_it);
+        unsigned edge_weight = get(boost::edge_name, g, *out_edge_it);
         add_edge(
-            new_vertex, target_vertex, EdgeProperty(1), *new_graph);
+            new_vertex, target_vertex, EdgeProperty(edge_weight), *new_graph);
         existing_nodes->operator[](target_id) = target_vertex;
         (*num_nodes_visited)++;
       }
     }
   }
-  void initialize_vertex(const Vertex& v, const Graph& g) const {}
-  void examine_vertex(const Vertex& v, const Graph& g) const {}
-  void examine_edge(const Edge& e, const Graph& g) const {}
-  void finish_vertex(const Vertex& v, const Graph& g) const {}
 
  private:
   std::map<unsigned, Vertex> *existing_nodes;
