e041287ee04e4db8c68f6160508d9c0b605e59f9
Sam Xi <samxi@seas.harvard.edu>
2017-10-09 22:09:37 -0400

    gem5: Don't allow dmaLoad/dmaStore of size 0.
    Change-Id: I53ab9011bcb85830c5dffcc0e45ce85e7e82f30b

--- gem5/dma_interface.c
+++ gem5/dma_interface.c
@@ -1,23 +1,28 @@
+#include <assert.h>
 #include <string.h>
 #include "dma_interface.h"
 
 #ifdef DMA_INTERFACE_V2
 void* dmaLoad(void* base_addr, size_t src_off, size_t dst_off, size_t size) {
+  assert(size > 0);
   memmove(base_addr + dst_off, base_addr + src_off, size);
   return NULL;
 }
 
 void* dmaStore(void* base_addr, size_t src_off, size_t dst_off, size_t size) {
+  assert(size > 0);
   memmove(base_addr + dst_off, base_addr + src_off, size);
   return NULL;
 }
 #else
 void* dmaLoad(void* addr, size_t offset, size_t size) {
+  assert(size > 0);
   asm("");
   return NULL;
 }
 
 void* dmaStore(void* addr, size_t offset, size_t size) {
+  assert(size > 0);
   asm("");
   return NULL;
 }
