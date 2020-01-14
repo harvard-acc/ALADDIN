a46f49c496f61fe2686324cfec624561b73a9906
Sam Xi <samxi@seas.harvard.edu>
2020-01-14 00:10:58 -0500

    common: Forgot to actually commit the new MemoryType header.
    Change-Id: I9f41f24c8c9589c9914dd60eb11e5822622a44c5

---
+++ common/MemoryType.h
@@ -0,0 +1,8 @@
+#ifndef __MEMORY_TYPE_H__
+#define __MEMORY_TYPE_H__
+
+// Defines how an array is mapped to the accelerator and how it can be accessed
+// via the host.
+enum MemoryType { spad, reg, dma, acp, cache };
+
+#endif
