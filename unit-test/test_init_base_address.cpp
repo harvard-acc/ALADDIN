b1817499db32251dd406c6edbd3f0691f907392b
Yuan Yao <yuanyao@seas.harvard.edu>
2020-01-14 17:44:07 -0500

    unit-tests: Update test_initbase.
    Change-Id: I5efabbcc3dc636140bc3cbce4e19ae95329efff0

--- unit-test/test_init_base_address.cpp
+++ unit-test/test_init_base_address.cpp
@@ -20,13 +20,13 @@ SCENARIO("Test initBaseAddress w/ c[i]=a[i] case", "[equal]") {
     WHEN("Test initBaseAddress()") {
       acc->initBaseAddress();
       THEN("The baseAddress of memory operations should be either "
-           "'a', 'b' or 'c' for Triad.") {
-        REQUIRE(prog.getBaseAddressLabel(3).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(5).compare("c") == 0);
-        REQUIRE(prog.getBaseAddressLabel(11).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(13).compare("c") == 0);
-        REQUIRE(prog.getBaseAddressLabel(19).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(21).compare("c") == 0);
+           "'a_acc', 'b_acc' or 'c_acc' for Triad.") {
+        REQUIRE(prog.getBaseAddressLabel(9).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("c_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(11).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(27).compare("c_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(21).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(39).compare("c_acc") == 0);
       }
     }
   }
@@ -48,13 +48,13 @@ SCENARIO("Test initBaseAddress w/ Triad", "[triad]") {
     WHEN("Test initBaseAddress()") {
       acc->initBaseAddress();
       THEN("The baseAddress of memory operations should be either "
-           "'a', 'b' or 'c' for Triad.") {
-        REQUIRE(prog.getBaseAddressLabel(3).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(5).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(9).compare("c") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1491).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1493).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1497).compare("c") == 0);
+           "'a_acc', 'b_acc' or 'c_acc' for Triad.") {
+        REQUIRE(prog.getBaseAddressLabel(9).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(11).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("c_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1533).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1535).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1539).compare("c_acc") == 0);
       }
     }
   }
@@ -76,9 +76,9 @@ SCENARIO("Test initBaseAddress w/ Reduction", "[reduction]") {
       acc->initBaseAddress();
       THEN("The baseAddress of memory operations should be 'in' for "
            "Reduction.") {
-        REQUIRE(prog.getBaseAddressLabel(4).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(12).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1020).compare("in") == 0);
+        REQUIRE(prog.getBaseAddressLabel(7).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1023).compare("in_acc") == 0);
       }
     }
   }
@@ -100,45 +100,16 @@ SCENARIO("Test initBaseAddress w/ Pp_scan", "[pp_scan]") {
       acc->initBaseAddress();
       THEN("The baseAddress of memory operations should be 'bucket' or 'sum' "
            "for pp_scan.") {
-        REQUIRE(prog.getBaseAddressLabel(11).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(13).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(15).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1463).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1465).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1467).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1488).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1491).compare("sum") == 0);
-        REQUIRE(prog.getBaseAddressLabel(3169).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(3167).compare("sum") == 0);
-      }
-    }
-  }
-}
-
-SCENARIO("Test initBaseAddress w/ sort-radix", "[sort-radix]") {
-  GIVEN("Test the mapping between global variables to function's local variables.") {
-    std::string bench("outputs/sort-radix");
-    std::string trace_file("inputs/sort-radix-trace.gz");
-    std::string config_file("inputs/config-sort-radix");
-
-    ScratchpadDatapath* acc;
-    Scratchpad* spad;
-    acc = new ScratchpadDatapath(bench, trace_file, config_file);
-    auto& prog = acc->getProgram();
-    acc->buildDddg();
-    acc->removeInductionDependence();
-    acc->removePhiNodes();
-    WHEN("Test initBaseAddress()") {
-      acc->initBaseAddress();
-      THEN("In the 1st update(): global b -> local b, global a -> local a.\n\
-           In the 2nd update(): global a -> local b, global b -> local a.\n\
-           In the 3rd update(): global b -> local b, global a -> local a.") {
-        REQUIRE(prog.getBaseAddressLabel(97169).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(97180).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(240417).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(240428).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(383665).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(383676).compare("b") == 0);
+        REQUIRE(prog.getBaseAddressLabel(9).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(25).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1463).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1474).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1485).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1496).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1559).compare("sum") == 0);
+        REQUIRE(prog.getBaseAddressLabel(2821).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(2822).compare("sum") == 0);
       }
     }
   }
