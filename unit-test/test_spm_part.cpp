abc1d7fd6c087935b122f7c6009da36a78fb19f6
Yuan Yao <yuanyao@seas.harvard.edu>
2020-01-16 12:28:15 -0500

    unit-tests: Update test_spm_part.
    Change-Id: I6f22f715f34b1382a05c6e70adf74b6c89281e11

--- unit-test/test_spm_part.cpp
+++ unit-test/test_spm_part.cpp
@@ -21,31 +21,31 @@ SCENARIO("Test scratchpadPartition w/ Triad", "[triad]") {
     acc->initBaseAddress();
     WHEN("Test scratchpadPartition()") {
       acc->scratchpadPartition();
-      THEN("The baseAddress of memory operations should be '*-0' or '*-1'.") {
-        REQUIRE(prog.getBaseAddressLabel(3).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(5).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(9).compare("c") == 0);
-        REQUIRE(prog.nodes.at(3)->get_partition_index() == 0);
-        REQUIRE(prog.nodes.at(5)->get_partition_index() == 0);
+      THEN("The partition index of a memory node should be 0 or 1.") {
+        REQUIRE(prog.getBaseAddressLabel(9).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(11).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("c_acc") == 0);
         REQUIRE(prog.nodes.at(9)->get_partition_index() == 0);
-        REQUIRE(prog.getBaseAddressLabel(15).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(17).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(21).compare("c") == 0);
-        REQUIRE(prog.nodes.at(15)->get_partition_index() == 1);
-        REQUIRE(prog.nodes.at(17)->get_partition_index() == 1);
+        REQUIRE(prog.nodes.at(11)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(15)->get_partition_index() == 0);
+        REQUIRE(prog.getBaseAddressLabel(21).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(23).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(27).compare("c_acc") == 0);
         REQUIRE(prog.nodes.at(21)->get_partition_index() == 1);
-        REQUIRE(prog.getBaseAddressLabel(27).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(29).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(33).compare("c") == 0);
-        REQUIRE(prog.nodes.at(27)->get_partition_index() == 0);
-        REQUIRE(prog.nodes.at(29)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(23)->get_partition_index() == 1);
+        REQUIRE(prog.nodes.at(27)->get_partition_index() == 1);
+        REQUIRE(prog.getBaseAddressLabel(33).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(35).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(39).compare("c_acc") == 0);
         REQUIRE(prog.nodes.at(33)->get_partition_index() == 0);
-        REQUIRE(prog.getBaseAddressLabel(1491).compare("a") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1493).compare("b") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1497).compare("c") == 0);
-        REQUIRE(prog.nodes.at(1491)->get_partition_index() == 0);
-        REQUIRE(prog.nodes.at(1493)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(35)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(39)->get_partition_index() == 0);
+        REQUIRE(prog.getBaseAddressLabel(1497).compare("a_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1499).compare("b_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1503).compare("c_acc") == 0);
         REQUIRE(prog.nodes.at(1497)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(1499)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(1503)->get_partition_index() == 0);
       }
     }
   }
@@ -66,17 +66,17 @@ SCENARIO("Test scratchpadPartition w/ Reduction", "[reduction]") {
     acc->initBaseAddress();
     WHEN("Test scratchpadPartition()") {
       acc->scratchpadPartition();
-      THEN("The baseAddress of memory operations should be '*-0/1/2/3'.") {
-        REQUIRE(prog.getBaseAddressLabel(4).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(12).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(20).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(28).compare("in") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1020).compare("in") == 0);
-        REQUIRE(prog.nodes.at(4)->get_partition_index() == 0);
-        REQUIRE(prog.nodes.at(12)->get_partition_index() == 1);
-        REQUIRE(prog.nodes.at(20)->get_partition_index() == 2);
-        REQUIRE(prog.nodes.at(28)->get_partition_index() == 3);
-        REQUIRE(prog.nodes.at(1020)->get_partition_index() == 3);
+      THEN("The partition index of a memory node should be 0, 1, 2 or 3.") {
+        REQUIRE(prog.getBaseAddressLabel(7).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(23).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(31).compare("in_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1023).compare("in_acc") == 0);
+        REQUIRE(prog.nodes.at(7)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(15)->get_partition_index() == 1);
+        REQUIRE(prog.nodes.at(23)->get_partition_index() == 2);
+        REQUIRE(prog.nodes.at(31)->get_partition_index() == 3);
+        REQUIRE(prog.nodes.at(1023)->get_partition_index() == 3);
       }
     }
   }
@@ -97,27 +97,25 @@ SCENARIO("Test scratchpadPartition w/ pp_scan", "[pp_scan]") {
     acc->initBaseAddress();
     WHEN("Test scratchpadPartition()") {
       acc->scratchpadPartition();
-      THEN("The baseAddress of memory operations should be '*-0/1/2/3'.") {
-        REQUIRE(prog.getBaseAddressLabel(11).compare("bucket") == 0);
-        REQUIRE(prog.nodes.at(11)->get_partition_index() == 0);
-        REQUIRE(prog.getBaseAddressLabel(13).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(15).compare("bucket") == 0);
-        REQUIRE(prog.nodes.at(13)->get_partition_index() == 1);
+      THEN("The partition index of a memory node should be 0, 1, 2 or 3.") {
+        REQUIRE(prog.getBaseAddressLabel(9).compare("bucket_acc") == 0);
+        REQUIRE(prog.nodes.at(9)->get_partition_index() == 0);
+        REQUIRE(prog.getBaseAddressLabel(15).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(17).compare("bucket_acc") == 0);
         REQUIRE(prog.nodes.at(15)->get_partition_index() == 1);
-        REQUIRE(prog.getBaseAddressLabel(1463).compare("bucket") == 0);
-        REQUIRE(prog.nodes.at(1463)->get_partition_index() == 2);
-        REQUIRE(prog.getBaseAddressLabel(1465).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1467).compare("bucket") == 0);
-        REQUIRE(prog.getBaseAddressLabel(1488).compare("bucket") == 0);
-        REQUIRE(prog.nodes.at(1465)->get_partition_index() == 3);
-        REQUIRE(prog.nodes.at(1467)->get_partition_index() == 3);
-        REQUIRE(prog.nodes.at(1488)->get_partition_index() == 3);
-        REQUIRE(prog.getBaseAddressLabel(1491).compare("sum") == 0);
-        REQUIRE(prog.nodes.at(1491)->get_partition_index() == 1);
-        REQUIRE(prog.getBaseAddressLabel(3169).compare("bucket") == 0);
-        REQUIRE(prog.nodes.at(3169)->get_partition_index() == 3);
-        REQUIRE(prog.getBaseAddressLabel(3167).compare("sum") == 0);
-        REQUIRE(prog.nodes.at(3167)->get_partition_index() == 3);
+        REQUIRE(prog.nodes.at(17)->get_partition_index() == 1);
+        REQUIRE(prog.getBaseAddressLabel(1462).compare("bucket_acc") == 0);
+        REQUIRE(prog.nodes.at(1462)->get_partition_index() == 0);
+        REQUIRE(prog.getBaseAddressLabel(1463).compare("bucket_acc") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1464).compare("sum") == 0);
+        REQUIRE(prog.getBaseAddressLabel(1488).compare("bucket2_acc") == 0);
+        REQUIRE(prog.nodes.at(1463)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(1464)->get_partition_index() == 0);
+        REQUIRE(prog.nodes.at(1488)->get_partition_index() == 0);
+        REQUIRE(prog.getBaseAddressLabel(2821).compare("bucket_acc") == 0);
+        REQUIRE(prog.nodes.at(2821)->get_partition_index() == 3);
+        REQUIRE(prog.getBaseAddressLabel(2822).compare("sum") == 0);
+        REQUIRE(prog.nodes.at(2822)->get_partition_index() == 3);
       }
     }
   }
